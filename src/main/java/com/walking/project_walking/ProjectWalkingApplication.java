311660b51d1e91c800f83230f5bbda7195dc7289
kkb <0furiousblade0@gmail.com>
2024-10-28 11:38:14 +0900

    [#01 ADD] 프로젝트 생성

---
+++ src/main/java/com/walking/project_walking/ProjectWalkingApplication.java
@@ -0,0 +1,13 @@
+package com.walking.project_walking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWalkingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectWalkingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/walking/project_walking/ProjectWalkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walking.project_walking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWalkingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

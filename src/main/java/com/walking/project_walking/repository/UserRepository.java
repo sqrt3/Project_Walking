65209173edca616d6d172970f6f1593fa3f92a45
greenapple <dla16253745@gmail.com>
2024-11-01 00:20:43 +0900

    Feat : 실제 존재하는 userId인지 검증하는 메소드 추가

--- src/main/java/com/walking/project_walking/repository/UserRepository.java
+++ src/main/java/com/walking/project_walking/repository/UserRepository.java
@@ -2,7 +2,11 @@ package com.walking.project_walking.repository;
 
 import com.walking.project_walking.domain.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 public interface UserRepository extends JpaRepository<Users, Long> {
+    @Query("SELECT u.userId FROM Users u WHERE u.nickname = :nickname")
+    Long getUserIdByNickname(@Param("nickname") String nickname);
 }
--- src/main/java/com/walking/project_walking/service/UserService.java
+++ src/main/java/com/walking/project_walking/service/UserService.java
@@ -7,14 +7,19 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-    private final UserRepository repository;
+    private final UserRepository userRepository;
 
     public UserService(UserRepository repository) {
-        this.repository = repository;
+        this.userRepository = repository;
     }
 
     // 유저 생성
     public Users saveUser(AddUserRequest request) {
-        return repository.save(request.toEntity());
+        return userRepository.save(request.toEntity());
+    }
+
+    // 실제 존재하는 userId인지 검증하는 메소드
+    public boolean existsById(Long userId) {
+        return userRepository.existsById(userId);
     }
 }

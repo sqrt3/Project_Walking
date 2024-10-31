5193cccace1e7770b6bd042be398cd944aaca215
pc <gkandpwpjp@gmail.com>
2024-10-31 09:45:14 +0900

    feat : 가입시 이메일 중복 체크

--- src/main/java/com/walking/project_walking/repository/UserRepository.java
+++ src/main/java/com/walking/project_walking/repository/UserRepository.java
@@ -4,5 +4,11 @@ import com.walking.project_walking.domain.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<Users, Long> {
+
+    boolean existsByEmail(String email);
+
+    Optional<Users> findByEmail(String email);
 }

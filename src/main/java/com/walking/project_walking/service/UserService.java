73e02804d3d1820f6710d8b5cca2e5d371d6c030
greenapple <dla16253745@gmail.com>
2024-11-01 00:52:30 +0900

    Fix : Merge 과정에서 충돌나는 부분 해결 및 repository를 userRepository로 변경

--- src/main/java/com/walking/project_walking/service/UserService.java
+++ src/main/java/com/walking/project_walking/service/UserService.java
@@ -16,12 +16,11 @@ import java.util.List;
 
 @Service
 public class UserService {
-    private final UserRepository repository;
     private final BCryptPasswordEncoder encoder;
     private final UserRepository userRepository;
 
-    public UserService(UserRepository repository, BCryptPasswordEncoder encoder) {
-        this.repository = repository;
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder encoder) {
+        this.userRepository = userRepository;
         this.encoder = encoder;
     }
 
@@ -40,31 +39,26 @@ public class UserService {
                 .joinDate(LocalDate.now())
                 .build();
 
-        return repository.save(user);
+        return userRepository.save(user);
     }
 
     public boolean checkEmailExists(String email) {
-        return repository.existsByEmail(email);
+        return userRepository.existsByEmail(email);
     }
 
     public boolean checkNicknameExists(String nickname) {
-        return repository.existsByNickname(nickname);
+        return userRepository.existsByNickname(nickname);
     }
 
     // 유저 정보 전체 조회
     public List<Users> findAll() {
-        return repository.findAll();
-    public UserService(UserRepository repository) {
-        this.userRepository = repository;
+        return userRepository.findAll();
     }
 
     // 유저 한 명 조회
     public Users findById(Long id) {
-        return repository.findById(id)
+        return userRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 유저입니다."));
-    // 유저 생성
-    public Users saveUser(AddUserRequest request) {
-        return userRepository.save(request.toEntity());
     }
 
     // 실제 존재하는 userId인지 검증하는 메소드
@@ -75,22 +69,22 @@ public class UserService {
     // 유저 정보 수정
     @Transactional
     public ResponseEntity<String> updateById(Long id, UserUpdate update) {
-        Users users = repository.findById(id)
+        Users users = userRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 유저입니다."));
 
         users.update(update.getPassword(), update.getPhone(), update.getNickname(), update.getProfileImage());
 
-        repository.save(users);
+        userRepository.save(users);
 
         return new ResponseEntity<>("수정이 완료되었습니다 :)", HttpStatus.OK);
     }
 
     @Transactional
     public void softDeleteUser(Long id) {
-        Users user = repository.findById(id)
+        Users user = userRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 유저입니다."));
         user.deactivate();
-        repository.save(user);
+        userRepository.save(user);
     }
 
 }

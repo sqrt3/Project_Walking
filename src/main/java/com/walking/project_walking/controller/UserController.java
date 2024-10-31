2c4a949accceeca90714ed1a3d30679d9772d791
pc <gkandpwpjp@gmail.com>
2024-10-31 10:50:31 +0900

    feat : 회원가입 기능 추가

--- src/main/java/com/walking/project_walking/controller/UserController.java
+++ src/main/java/com/walking/project_walking/controller/UserController.java
@@ -1,8 +1,8 @@
 package com.walking.project_walking.controller;
 
 import com.walking.project_walking.domain.Users;
-import com.walking.project_walking.domain.userdto.AddUserRequest;
 import com.walking.project_walking.domain.userdto.UserResponse;
+import com.walking.project_walking.domain.userdto.UserSignUpDto;
 import com.walking.project_walking.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,20 +10,23 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
+
 @RestController
 public class UserController {
-    private final UserService service;
+    private final UserService userservice;
 
-    public UserController(UserService service) {
-        this.service = service;
+    public UserController(UserService userservice) {
+        this.userservice = userservice;
     }
 
     @PostMapping("/users")
-    public ResponseEntity<UserResponse> createUser(
-            @RequestBody AddUserRequest request
-    ) {
-        Users users = service.saveUser(request);
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(new UserResponse(users));
+    public ResponseEntity<UserResponse> createUser(@RequestBody UserSignUpDto request) {
+        Users user = userservice.saveUser(request);
+        String redirectUri = "/auth/login";
+
+        UserResponse response = new UserResponse(user, redirectUri);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
--- src/main/java/com/walking/project_walking/service/UserService.java
+++ src/main/java/com/walking/project_walking/service/UserService.java
@@ -1,20 +1,45 @@
 package com.walking.project_walking.service;
 
 import com.walking.project_walking.domain.Users;
-import com.walking.project_walking.domain.userdto.AddUserRequest;
+
+import com.walking.project_walking.domain.userdto.UserSignUpDto;
+import com.walking.project_walking.exception.UserAlreadyExistsException;
 import com.walking.project_walking.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
 
 @Service
 public class UserService {
     private final UserRepository repository;
+    private final BCryptPasswordEncoder encoder;
 
-    public UserService(UserRepository repository) {
+    public UserService(UserRepository repository, BCryptPasswordEncoder encoder) {
         this.repository = repository;
+        this.encoder = encoder;
     }
 
-    // 유저 생성
-    public Users saveUser(AddUserRequest request) {
-        return repository.save(request.toEntity());
+    @Transactional
+    public Users saveUser(UserSignUpDto dto) {
+        if (repository.existsByEmail(dto.getEmail())) {
+            throw new UserAlreadyExistsException("이미 사용 중인 이메일입니다.");
+        }
+
+        String encodedPassword = encoder.encode(dto.getPassword());
+
+        Users user = Users.builder()
+                .email(dto.getEmail())
+                .password(encodedPassword)
+                .name(dto.getName())
+                .phone(dto.getPhone())
+                .nickname(dto.getNickname())
+                .gender(dto.getGender())
+                .birth(dto.getBirth())
+                .joinDate(LocalDate.now()) // 현재 날짜로 설정
+                .build();
+
+        return repository.save(user);
     }
 }

c6ffc8e0c099efb9fcc77c2e3bedf7ac1a41fd5b
pc <gkandpwpjp@gmail.com>
2024-10-31 10:56:31 +0900

    chore : 비밀번호 암호화를 위한 SecurityConfig 추가

--- src/main/java/com/walking/project_walking/repository/UserRepository.java
+++ src/main/java/com/walking/project_walking/repository/UserRepository.java
@@ -2,7 +2,6 @@ package com.walking.project_walking.repository;
 
 import com.walking.project_walking.domain.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 

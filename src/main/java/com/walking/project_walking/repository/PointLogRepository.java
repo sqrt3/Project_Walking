93d7246b52950ba9e35c54cc00c1d89902723079
sqrt3 <1dimchae@gmail.com>
2024-10-31 15:41:18 +0900

    refactor: PointLogRepository @Repository 어노테이션 추가

--- src/main/java/com/walking/project_walking/repository/PointLogRepository.java
+++ src/main/java/com/walking/project_walking/repository/PointLogRepository.java
@@ -2,7 +2,9 @@ package com.walking.project_walking.repository;
 
 import com.walking.project_walking.domain.PointLog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PointLogRepository extends JpaRepository<PointLog, Long> {
 
 }

7e31e3a61faa2e03dd84834645cbddad871c3c1b
kkb <0furiousblade0@gmail.com>
2024-10-31 09:49:14 +0900

    feat : 특정 user 조회 기능 추가

--- src/main/java/com/walking/project_walking/controller/UsersController.java
+++ src/main/java/com/walking/project_walking/controller/UsersController.java
@@ -1,9 +1,12 @@
 package com.walking.project_walking.controller;
 
+import com.walking.project_walking.domain.Users;
 import com.walking.project_walking.domain.userdto.UserResponse;
 import com.walking.project_walking.service.UserService;
+import org.apache.catalina.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -25,5 +28,11 @@ public class UsersController {
         return ResponseEntity.ok(list);
     }
 
+    // User 한 명 조회
+    @GetMapping("/users/{usersId}")
+    public ResponseEntity<UserResponse> findUserById(@PathVariable Long usersId) {
+        Users users = service.findById(usersId);
+        return ResponseEntity.ok(new UserResponse(users));
+    }
 
 }

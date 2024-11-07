693fa379aadc32a78be5e1cde141bdca9573ed40
greenapple <dla16253745@gmail.com>
2024-11-07 15:04:02 +0900

    Feat : S3에 이미지 저장 시 발생할 수 있는 예외를 다루는 코드

---
+++ src/main/java/com/walking/project_walking/exception/S3Exception.java
@@ -0,0 +1,19 @@
+package com.walking.project_walking.exception;
+
+public class S3Exception extends RuntimeException {
+    private final ErrorCode errorCode;
+
+    public S3Exception(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
+
+    public S3Exception(ErrorCode errorCode, Throwable cause) {
+        super(errorCode.getMessage(), cause);
+        this.errorCode = errorCode;
+    }
+}

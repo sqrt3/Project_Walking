288f9e7c7189f2f8a2f064033f1e1930aac6ccc2
sqrt3 <1dimchae@gmail.com>
2024-11-09 15:09:01 +0900

    refactor: 사용하지 않는 import 삭제

--- src/main/java/com/walking/project_walking/controller/GoodsViewController.java
+++ src/main/java/com/walking/project_walking/controller/GoodsViewController.java
@@ -1,16 +1,12 @@
 package com.walking.project_walking.controller;
 
-import com.walking.project_walking.domain.Goods;
 import com.walking.project_walking.domain.Users;
 import com.walking.project_walking.domain.dto.GoodsResponseDto;
-import com.walking.project_walking.domain.userdto.UserResponse;
 import com.walking.project_walking.service.GoodsService;
 import com.walking.project_walking.service.UserService;
 import jakarta.servlet.http.HttpSession;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;

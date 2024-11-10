a4935812a0db4cb4ea193c64a9d468ed777c792d
shanan9623 <shanan9623@gmail.com>
2024-11-11 05:03:07 +0900

    Add: 이미지 삭제 기능 추가

--- src/main/java/com/walking/project_walking/repository/PostImagesRepository.java
+++ src/main/java/com/walking/project_walking/repository/PostImagesRepository.java
@@ -2,9 +2,11 @@ package com.walking.project_walking.repository;
 
 import com.walking.project_walking.domain.PostImages;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,4 +14,11 @@ import java.util.List;
 public interface PostImagesRepository extends JpaRepository<PostImages, Long> {
     @Query("SELECT p.imageUrl FROM PostImages p WHERE p.postId = :postId")
     List<String> findImageUrlsByPostId(@Param("postId") Long postId);
+
+    // postId와 연관된 모든 이미지 삭제
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM PostImages p WHERE p.postId = :postId")
+    void deleteByPostId(@Param("postId") Long postId);
 }
+
